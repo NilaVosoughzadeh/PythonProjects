d2274f3833daf8e0b2125be96f80834206da5c2d
Nila <nilavosoughzadeh@gmail.com>
2025-01-28 15:06:49 +0330

    DiceRoller
    DiceRoller is a simple yet engaging Python application that simulates the roll of a six-sided dice. Each roll generates a random number between 1 and 6, and the corresponding dice face is displayed in a different color, enhancing the visual experience. The program continues to roll until the user decides to stop.

---
+++ diceRoller.py
@@ -0,0 +1,68 @@
+from random import randint
+from colorama import Fore
+
+x = "y"
+
+while x == "y":
+    number = randint(1,6)
+
+    if number == 1:
+        print(Fore.LIGHTRED_EX)
+        print("===========")
+        print("|         |")
+        print("|    0    |")
+        print("|         |")
+        print("===========")
+        print(Fore.RESET)
+
+    elif number == 2:
+        print(Fore.LIGHTYELLOW_EX)
+        print("===========")
+        print("|         |")
+        print("|  0   0  |")
+        print("|         |")
+        print("===========")
+        print(Fore.RESET)
+
+    elif number == 3:
+        print(Fore.LIGHTMAGENTA_EX)
+        print("===========")
+        print("|       0 |")
+        print("|    0    |")
+        print("| 0       |")
+        print("===========")
+        print(Fore.RESET)
+
+    elif number == 4:
+        print(Fore.LIGHTGREEN_EX)
+        print("===========")
+        print("| 0     0 |")
+        print("|         |")
+        print("| 0     0 |")
+        print("===========")
+        print(Fore.RESET)
+
+    elif number == 5:
+        print(Fore.LIGHTCYAN_EX)
+        print("===========")
+        print("| 0     0 |")
+        print("|    0    |")
+        print("| 0     0 |")
+        print("===========")
+        print(Fore.RESET)
+
+    elif number == 6:
+        print(Fore.LIGHTBLUE_EX)
+        print("===========")
+        print("| 0  0  0 |")
+        print("|         |")
+        print("| 0  0  0 |")
+        print("===========")
+        print(Fore.RESET)
+
+    x = input("Do you want to continue? ")
+
+    if x == "y":
+        continue
+    else:
+        break
