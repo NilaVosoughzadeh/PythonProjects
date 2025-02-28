7b7d6f114ede335a04052151fb73d9b2b5e4eed4
Nila <nilavosoughzadeh@gmail.com>
2025-03-01 00:22:28 +0330

    RandomShuffleChoice

---
+++ random.py
@@ -0,0 +1,11 @@
+import random
+random.randint(1 , 100)
+
+#بر زدن اعداد لیست
+l=[1,2,3,4]
+random.shuffle(l)
+print(l)
+
+#انتخاب کردن یک مورد به صورت شانسی
+lName = ["Nila" , "Arash" , "Minoo" , "Shahram"]
+random.choice(lName)
